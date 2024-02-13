ce571254eaa5391fc291e3919af65fa5d1987173
Hamzah SM <hamzahsaiful782@gmail.com>
2024-02-13 11:03:16 +0700

    landing-page-sudah-ok

--- resources/views/partials/properti-terpopuler.blade.php
+++ resources/views/partials/properti-terpopuler.blade.php
@@ -1,29 +1,23 @@
 	<!-- Intro -->
-	
 	<div class="intro">
 		<div class="container">
 			<div class="row">
 				<div class="col">
-					<h2 class="intro_title text-start fs-3">Daftar Properti Baru</h2>
-                    <p class="intro_text text-start text-dark">Cari hunian impian Anda, dari rumah minimalis hingga apartemen mewah di di</p>
+					<h2 class="intro_title text-start fs-3">Properti Terpopuler</h2>
+                    <p class="intro_text text-start text-dark">Iklan properti terpopuler dengan lokasi strategis dan harga menarik</p>
 				</div>
 			</div>
 
             <div class="product_slider">
-                <div class="owl-carousel owl-theme product_slider_home">
-                    @foreach ($ads as $item)
+                <div class="owl-carousel owl-theme properti_terpopuler">
+                    @foreach ($premiums as $item)
                         <div class="item p-3">
                             <div class="intro_item">
                                 <div class="intro_item_overlay"></div>
                                 <div class="intro_item_background" style="background-image:url({{ Storage::url($item->featured_image) }})"></div>
                                 <div class="intro_item_content d-flex flex-column align-items-center justify-content-center">
                                     <div class="intro_date">{{ $item->judul_properti }}</div>
-                                    {{-- <div class="intro_date">Diuplod : {{ $item->created_at }}</div> --}}
-                                    <div class="button intro_button">
-                                        <div class="button_bcg"></div>
-                                        {{-- <a href="{{ route('properties.show', $item->id) }}"><i class="fa fa-external-link" aria-hidden="true"></i> {{ $item->harga_start_properti }} - {{ $item->harga_end_properti }}</a> --}}
-                                        <a href="/single-listing/{{ $item->id }}"><i class="fa fa-external-link" aria-hidden="true"></i> {{ $item->harga_start_properti }} - {{ $item->harga_end_properti }}</a>
-                                    </div>
+                                    <div class="button intro_button"><div class="button_bcg"></div><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> {{ $item->harga_start_properti }} - {{ $item->harga_end_properti }}</a></div>
                                     <ul class="text-white">
                                         <li><i class="fa fa-bed" aria-hidden="true"></i> : {{ $item->kamar_tidur_properti }}</li>
                                         <li><i class="fa fa-bath" aria-hidden="true"></i> : {{ $item->kamar_mandi_properti }}</li>
@@ -32,7 +26,8 @@
                                     </ul>
                                 </div>
                             </div>
-                        </div>   
+                        </div>
+                        
                     @endforeach
     
                 </div>
--- resources/views/partials/testimonial.blade.php
+++ resources/views/partials/testimonial.blade.php
@@ -14,15 +14,14 @@
 							<!-- Testimonial Item -->
 							<div class="owl-item">
 								<div class="test_item">
-									<div class="test_image"><img src="images/test_1.jpg" alt="https://unsplash.com/@anniegray"></div>
+									<div class="test_image"><img src="{{ asset('images/testimoni/testimoni_1.jpg') }}" alt="https://unsplash.com/@anniegray"></div>
 									<div class="test_content_container">
 										<div class="test_content">
 											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
+												<div class="test_name">Danang</div>
+												<div class="test_date">Agen</div>
 											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
+											<p class="test_quote_text">Rumah membantu saya mendapatkan leads dari customer dengan optimal. Dengan memanfaatkan Feature Listing, calon pembeli datang setiap harinya. Selain mendapatkan prospek untuk calon pembeli, saya juga mendapatkan leads untuk riset pengembangan properti saya.</p>
 										</div>
 									</div>
 								</div>
@@ -31,15 +30,14 @@
 							<!-- Testimonial Item -->
 							<div class="owl-item">
 								<div class="test_item">
-									<div class="test_image"><img src="images/test_2.jpg" alt="https://unsplash.com/@tschax"></div>
+									<div class="test_image"><img src="{{ asset('images/testimoni/testimoni_2.jpg') }}" alt="#"></div>
 									<div class="test_content_container">
 										<div class="test_content">
 											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
+												<div class="test_name">Ananta</div>
+												<div class="test_date">Pembeli</div>
 											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
+											<p class="test_quote_text">situsnya bagus... membantu dalam memberikan informasi untuk mencari rumah khususnya buat saya yang suka cari2 informasi melalui hp... suksess ya untuk rumah... dan terus membantu untuk para pencari hunian seperti saya</p>
 										</div>
 									</div>
 								</div>
@@ -48,108 +46,20 @@
 							<!-- Testimonial Item -->
 							<div class="owl-item">
 								<div class="test_item">
-									<div class="test_image"><img src="images/test_3.jpg" alt="https://unsplash.com/@seefromthesky"></div>
+									<div class="test_image"><img src="{{ asset('images/testimoni/testimoni_3.jpg') }}" alt="#"></div>
 									<div class="test_content_container">
 										<div class="test_content">
 											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
+												<div class="test_name">Adeline</div>
+												<div class="test_date">Agen</div>
 											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="images/test_2.jpg" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="images/test_1.jpg" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="images/test_3.jpg" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
+											<p class="test_quote_text">Saya mendapatkan lebih banyak buyer setelah mengiklankan properti di Rumah123. Interface Rumah123 memudahkan saya mengatur promosi listing dengan baik.</p>
 										</div>
 									</div>
 								</div>
 							</div>
 
 						</div>
-
-						<!-- Testimonials Slider Nav - Prev -->
-						{{-- <div class="test_slider_nav test_slider_prev">
-							<svg version="1.1" id="Layer_6" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-								width="28px" height="33px" viewBox="0 0 28 33" enable-background="new 0 0 28 33" xml:space="preserve">
-								<defs>
-									<linearGradient id='test_grad_prev'>
-										<stop offset='0%' stop-color='#fa9e1b'/>
-										<stop offset='100%' stop-color='#8d4fff'/>
-									</linearGradient>
-								</defs>
-								<path class="nav_path" fill="#F3F6F9" d="M19,0H9C4.029,0,0,4.029,0,9v15c0,4.971,4.029,9,9,9h10c4.97,0,9-4.029,9-9V9C28,4.029,23.97,0,19,0z
-								M26,23.091C26,27.459,22.545,31,18.285,31H9.714C5.454,31,2,27.459,2,23.091V9.909C2,5.541,5.454,2,9.714,2h8.571
-								C22.545,2,26,5.541,26,9.909V23.091z"/>
-								<polygon class="nav_arrow" fill="#F3F6F9" points="15.044,22.222 16.377,20.888 12.374,16.885 16.377,12.882 15.044,11.55 9.708,16.885 11.04,18.219 
-								11.042,18.219 "/>
-							</svg>
-						</div>
-						
-						<!-- Testimonials Slider Nav - Next -->
-						<div class="test_slider_nav test_slider_next">
-							<svg version="1.1" id="Layer_7" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-							width="28px" height="33px" viewBox="0 0 28 33" enable-background="new 0 0 28 33" xml:space="preserve">
-								<defs>
-									<linearGradient id='test_grad_next'>
-										<stop offset='0%' stop-color='#fa9e1b'/>
-										<stop offset='100%' stop-color='#8d4fff'/>
-									</linearGradient>
-								</defs>
-							<path class="nav_path" fill="#F3F6F9" d="M19,0H9C4.029,0,0,4.029,0,9v15c0,4.971,4.029,9,9,9h10c4.97,0,9-4.029,9-9V9C28,4.029,23.97,0,19,0z
-							M26,23.091C26,27.459,22.545,31,18.285,31H9.714C5.454,31,2,27.459,2,23.091V9.909C2,5.541,5.454,2,9.714,2h8.571
-							C22.545,2,26,5.541,26,9.909V23.091z"/>
-							<polygon class="nav_arrow" fill="#F3F6F9" points="13.044,11.551 11.71,12.885 15.714,16.888 11.71,20.891 13.044,22.224 18.379,16.888 17.048,15.554 
-							17.046,15.554 "/>
-							</svg>
-						</div> --}}
 
 					</div>
 					
