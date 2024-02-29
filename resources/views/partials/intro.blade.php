edd44553a46a50e24cc93b96ebd93777a2b65e62
Hamzah SM <hamzahsaiful782@gmail.com>
2024-02-29 14:40:08 +0700

    new-ui-intor-landing

--- resources/views/partials/intro.blade.php
+++ resources/views/partials/intro.blade.php
@@ -12,8 +12,20 @@
             <div class="product_slider">
                 <div class="owl-carousel owl-theme product_slider_home">
                     @foreach ($ads as $item)
-                        <div class="item p-3">
-                            <div class="intro_item">
+                        {{-- <div class="item p-3"> --}}
+                        <a href="/single-listing/{{ $item->id }}">
+                            <div class="card">
+                                <img src="{{ Storage::url($item->featured_image) }}" class="card-img-top img-fluid" alt="...">
+                                <div class="card-body text-dark">
+                                <h5 class="card-title font-weight-bold">{{ $item->judul_properti }}</h5>
+                                <h5 class="card-title"><span class="font-weight-bold">Lokasi :</span> {{ $item->lokasi_properti }} - {{ $item->provinsi_properti }}</h5>
+                                <h5 class="card-title"><span class="font-weight-bold">Harga :</span>{{ $item->harga_start_properti }} - {{ $item->harga_end_properti }}</h5>
+                                <h5 class="card-title"><span class="font-weight-bold">LT :</span> {{ $item->luas_tanah_properti }}</h5>
+                                <h5 class="card-title"><span class="font-weight-bold">LB :</span> {{ $item->luas_bangunan_properti }}</h5>
+                                </div>
+                            </div>
+                        </a>
+                            {{-- <div class="intro_item">
                                 <div class="intro_item_overlay"></div>
                                 <div class="intro_item_background" style="background-image:url({{ Storage::url($item->featured_image) }})"></div>
                                 <div class="intro_item_content d-flex flex-column align-items-center justify-content-center">
@@ -29,11 +41,11 @@
                                         <li>LB : {{ $item->luas_bangunan_properti }}</li>
                                     </ul>
                                 </div>
-                            </div>
-                        </div>   
+                            </div> --}}
+                        {{-- </div>    --}}
                     @endforeach
-    
                 </div>
             </div>
-		</div>
+
+		</div> 
 	</div>
